12deaae9cb447f0740bbc92b3bd4f89e4b6022a7
Jake Martinez <jake.martinez316@gmail.com>
2019-07-01 16:04:47 -0400

    Update with MutationObserver

--- progressive-img.js
+++ progressive-img.js
@@ -1,77 +1,103 @@
 
 
-(function() {
+export default function init(settings=null) {
     const domReady = function (callback) {
         document.readyState === 'interactive' || document.readyState === 'complete' ? callback() : document.addEventListener('DOMContentLoaded', callback)
     }
 
     domReady(() => {
-        if (typeof IntersectionObserver !== 'undefined') {
-        	// lazy load images in img tags when scrolled into view
-            const ioImgs = new IntersectionObserver(
-                entries => {
-                    console.log(entries)
+        const watch = () => {
+            if (typeof IntersectionObserver !== 'undefined') {
+                const ioImgs = new IntersectionObserver(
+                    entries => {
+                        // console.log(entries)
 
-                    Object.keys(entries).forEach(k => {
-                        if (!entries[k].isIntersecting) {
-                            return;
-                        }
-                        const el = entries[k].target
-                        el.src = el.getAttribute('data-src')
-                        ioImgs.unobserve(el)
-                    })
-                },
-                {
-                    /* Using default options. */
-                }
-            )
+                        Object.keys(entries).forEach(k => {
+                            if (!entries[k].isIntersecting) {
+                                return;
+                            }
+                            const el = entries[k].target
+                            const src = el.getAttribute('data-src')
+                            el.src = src
+                            ioImgs.unobserve(el)
+                        })
+                    },
+                    {
+                        /* Using default options. */
+                    }
+                )
 
-            // bind image elements to observer
-            const imgs = document.querySelectorAll('img[data-src]')
-            Object.keys(imgs).forEach(k => {
-                const el = imgs[k]
-                ioImgs.observe(el)
-            })
+                const imgs = document.querySelectorAll('.progressive-img img[data-src]')
+                Object.keys(imgs).forEach(k => {
+                    const el = imgs[k]
+                    ioImgs.observe(el)
+                })
 
 
-            // lazy load background images when scrolled into view
-            const ioBgImgs = new IntersectionObserver(
-                entries => {
-                    console.log(entries)
+                const ioBgImgs = new IntersectionObserver(
+                    entries => {
+                        // console.log(entries)
 
-                    Object.keys(entries).forEach(k => {
-                        if (!entries[k].isIntersecting) {
-                            return;
-                        }
-                        const el = entries[k].target
-                        el.setAttribute('style', `background-image: url('${el.getAttribute('data-bg-src')}');`)
-                        ioBgImgs.unobserve(el)
-                    })
-                },
-                {
-                    /* Using default options. */
-                }
-            )
+                        Object.keys(entries).forEach(k => {
+                            if (!entries[k].isIntersecting) {
+                                return;
+                            }
+                            const el = entries[k].target
+                            const src = el.getAttribute('data-src')
+                            el.setAttribute('style', `background-image: url('${src}');`)
+                            ioBgImgs.unobserve(el)
+                        })
+                    },
+                    {
+                        /* Using default options. */
+                    }
+                )
+                const bgImgs = document.querySelectorAll('.progressive-img div[data-src]')
+                Object.keys(bgImgs).forEach(k => {
+                    const el = bgImgs[k]
+                    ioBgImgs.observe(el)
+                })
+            } else {
+                const imgs = document.querySelectorAll('.progressive-img img[data-src]')
+                Object.keys(imgs).forEach(k => {
+                    const el = imgs[k]
+                    const src = el.getAttribute('data-src')
+                    el.src = src
+                })
 
-            // bind elements with background images to observer
-            const bgImgs = document.querySelectorAll('[data-bg-src]')
-            Object.keys(bgImgs).forEach(k => {
-                const el = bgImgs[k]
-                ioBgImgs.observe(el)
-            })
-        } else {
-        	// if IntersectionObserver isn't supported, then just load all of the images
-            const imgs = document.querySelectorAll('img[data-src]')
-            Object.keys(imgs).forEach(k => {
-                const el = imgs[k]
-                el.src = el.getAttribute('data-src')
-            })
-
-            const bgImgs = document.querySelectorAll('[data-bg-src]')
-            Object.keys(bgImgs).forEach(k => {
-                const el = bgImgs[k]
-                el.setAttribute('style', `background-image: url('${el.getAttribute('data-src')}');`)
-            })
+                const bgImgs = document.querySelectorAll('.progressive-img div[data-src]')
+                Object.keys(bgImgs).forEach(k => {
+                    const el = bgImgs[k]
+                    const src = el.getAttribute('data-src')
+                    el.setAttribute('style', `background-image: url('${src}');`)
+                })
+            }
         }
+
+        // create callback for use with MutationObserver
+        const callback = (mutationsList, observer) => {
+            const elementsWereAdded = (list) => {
+                for (let i = 0; i < list.length; i++) {
+                    if (list[i].addedNodes && list[i].addedNodes.length > 0) {
+                        return true
+                    }
+                }
+                return false
+            }
+            if (elementsWereAdded(mutationsList)) {
+                watch()
+            }
+        }
+
+        // if supported, add a mutation observer for all body DOM nodes
+        if (typeof MutationObserver !== 'undefined') {
+            const obs = new MutationObserver(callback)
+            const root = document.getElementsByTagName('body')[0]
+            const config = { childList: true, subtree: true };
+            obs.observe(root, config)
+        }
+
+        // initial watch event
+        watch()
     })
-})();
+}
