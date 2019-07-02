9b6a740674d4e7032d03ec8507a0e1e984c10af3
Jake Martinez <jake.martinez316@gmail.com>
2019-07-02 10:03:20 -0400

    Fix mutation observing functionality

--- compatible.js
+++ compatible.js
@@ -1,110 +1,114 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.default = init;
 
 function init() {
-    var settings = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+  var settings = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
 
-    var domReady = function domReady(callback) {
-        document.readyState === 'interactive' || document.readyState === 'complete' ? callback() : document.addEventListener('DOMContentLoaded', callback);
+  var domReady = function domReady(callback) {
+    document.readyState === 'interactive' || document.readyState === 'complete' ? callback() : document.addEventListener('DOMContentLoaded', callback);
+  };
+
+  domReady(function () {
+    var watch = function watch(nodeList, observer) {
+      Object.keys(nodeList).forEach(function (k) {
+        var el = nodeList[k];
+        observer.observe(el);
+      });
     };
 
-    domReady(function () {
-        var watch = function watch() {
-            if (typeof IntersectionObserver !== 'undefined') {
-                var ioImgs = new IntersectionObserver(function (entries) {
-                    // console.log(entries)
-                    Object.keys(entries).forEach(function (k) {
-                        if (!entries[k].isIntersecting) {
-                            return;
-                        }
+    if (typeof IntersectionObserver !== 'undefined') {
+      // regular images observer
+      var ioImgs = new IntersectionObserver(function (entries) {
+        // console.log(entries)
+        Object.keys(entries).forEach(function (k) {
+          if (!entries[k].isIntersecting) {
+            return;
+          }
 
-                        var el = entries[k].target;
-                        var src = el.getAttribute('data-src');
-                        el.src = src;
-                        el.removeAttribute('data-src');
-                        ioImgs.unobserve(el);
-                    });
-                }, {
-                    /* Using default options. */
-                });
-                var imgs = document.querySelectorAll('.progressive-img img[data-src]');
-                Object.keys(imgs).forEach(function (k) {
-                    var el = imgs[k];
-                    ioImgs.observe(el);
-                });
-                var ioBgImgs = new IntersectionObserver(function (entries) {
-                    // console.log(entries)
-                    Object.keys(entries).forEach(function (k) {
-                        if (!entries[k].isIntersecting) {
-                            return;
-                        }
+          var el = entries[k].target;
+          var src = el.getAttribute('data-src');
+          el.src = src;
+          el.removeAttribute('data-src');
+          ioImgs.unobserve(el);
+        });
+      }, {
+        /* Using default options. */
+      }); // background images observer
 
-                        var el = entries[k].target;
-                        var src = el.getAttribute('data-src');
-                        el.removeAttribute('data-src');
-                        el.setAttribute('style', "background-image: url('".concat(src, "');"));
-                        ioBgImgs.unobserve(el);
-                    });
-                }, {
-                    /* Using default options. */
-                });
-                var bgImgs = document.querySelectorAll('.progressive-img div[data-src]');
-                Object.keys(bgImgs).forEach(function (k) {
-                    var el = bgImgs[k];
-                    ioBgImgs.observe(el);
-                });
-            } else {
-                var _imgs = document.querySelectorAll('.progressive-img img[data-src]');
+      var ioBgImgs = new IntersectionObserver(function (entries) {
+        // console.log(entries)
+        Object.keys(entries).forEach(function (k) {
+          if (!entries[k].isIntersecting) {
+            return;
+          }
 
-                Object.keys(_imgs).forEach(function (k) {
-                    var el = _imgs[k];
-                    var src = el.getAttribute('data-src');
-                    el.src = src;
-                });
+          var el = entries[k].target;
+          var src = el.getAttribute('data-src');
+          el.setAttribute('style', "background-image: url('".concat(src, "');"));
+          el.removeAttribute('data-src');
+          ioBgImgs.unobserve(el);
+        });
+      }, {
+        /* Using default options. */
+      });
+      var imgs = document.querySelectorAll('.progressive-img img[data-src]');
+      watch(imgs, ioImgs);
+      var bgImgs = document.querySelectorAll('.progressive-img div[data-src]');
+      watch(bgImgs, ioBgImgs); // create callback for use with MutationObserver
 
-                var _bgImgs = document.querySelectorAll('.progressive-img div[data-src]');
+      var callback = function callback(mutationsList, observer) {
+        console.log('mutationsList', mutationsList);
 
-                Object.keys(_bgImgs).forEach(function (k) {
-                    var el = _bgImgs[k];
-                    var src = el.getAttribute('data-src');
-                    el.setAttribute('style', "background-image: url('".concat(src, "');"));
-                });
-            }
-        }; // create callback for use with MutationObserver
+        for (var i = 0; i < mutationsList.length; i++) {
+          if (mutationsList[i].addedNodes && mutationsList[i].addedNodes.length > 0) {
+            (function () {
+              var nodes = mutationsList[i].addedNodes;
+              Object.keys(nodes).forEach(function (k) {
+                var node = nodes[k];
 
-
-        var callback = function callback(mutationsList, observer) {
-            var elementsWereAdded = function elementsWereAdded(list) {
-                for (var i = 0; i < list.length; i++) {
-                    if (list[i].addedNodes && list[i].addedNodes.length > 0) {
-                        return true;
-                    }
+                if (node.nodeType === Node.ELEMENT_NODE) {
+                  var addedImgs = node.querySelectorAll('.progressive-img img[data-src]');
+                  watch(addedImgs, ioImgs);
+                  var addedBgImgs = node.querySelectorAll('.progressive-img div[data-src]');
+                  watch(addedBgImgs, ioBgImgs);
                 }
-
-                return false;
-            };
-
-            if (elementsWereAdded(mutationsList)) {
-                watch();
-            }
-        }; // if supported, add a mutation observer for all body DOM nodes
+              });
+            })();
+          }
+        }
+      }; // if supported, add a mutation observer for all body DOM nodes
 
 
-        if (typeof MutationObserver !== 'undefined') {
-            var obs = new MutationObserver(callback);
-            var root = document.getElementsByTagName('body')[0];
-            var config = {
-                childList: true,
-                subtree: true
-            };
-            obs.observe(root, config);
-        } // initial watch event
+      if (typeof MutationObserver !== 'undefined') {
+        var obs = new MutationObserver(callback);
+        var root = document.getElementsByTagName('body')[0];
+        var config = {
+          childList: true,
+          subtree: true
+        };
+        obs.observe(root, config);
+      }
+    } else {
+      // if IntersectionObserver is not supported, just replace all images
+      var _imgs = document.querySelectorAll('.progressive-img img[data-src]');
 
+      Object.keys(_imgs).forEach(function (k) {
+        var el = _imgs[k];
+        var src = el.getAttribute('data-src');
+        el.src = src;
+      });
 
-        watch();
-    });
+      var _bgImgs = document.querySelectorAll('.progressive-img div[data-src]');
+
+      Object.keys(_bgImgs).forEach(function (k) {
+        var el = _bgImgs[k];
+        var src = el.getAttribute('data-src');
+        el.setAttribute('style', "background-image: url('".concat(src, "');"));
+      });
+    }
+  });
 }
--- progressive-img.js
+++ progressive-img.js
@@ -6,100 +6,106 @@ export default function init(settings=null) {
     }
 
     domReady(() => {
-        const watch = () => {
-            if (typeof IntersectionObserver !== 'undefined') {
-                const ioImgs = new IntersectionObserver(
-                    entries => {
-                        // console.log(entries)
-
-                        Object.keys(entries).forEach(k => {
-                            if (!entries[k].isIntersecting) {
-                                return;
-                            }
-                            const el = entries[k].target
-                            const src = el.getAttribute('data-src')
-                            el.src = src
-                            el.removeAttribute('data-src')
-                            ioImgs.unobserve(el)
-                        })
-                    },
-                    {
-                        /* Using default options. */
-                    }
-                )
-
-                const imgs = document.querySelectorAll('.progressive-img img[data-src]')
-                Object.keys(imgs).forEach(k => {
-                    const el = imgs[k]
-                    ioImgs.observe(el)
-                })
-
-
-                const ioBgImgs = new IntersectionObserver(
-                    entries => {
-                        // console.log(entries)
-
-                        Object.keys(entries).forEach(k => {
-                            if (!entries[k].isIntersecting) {
-                                return;
-                            }
-                            const el = entries[k].target
-                            const src = el.getAttribute('data-src')
-                            el.removeAttribute('data-src')
-                            el.setAttribute('style', `background-image: url('${src}');`)
-                            ioBgImgs.unobserve(el)
-                        })
-                    },
-                    {
-                        /* Using default options. */
-                    }
-                )
-                const bgImgs = document.querySelectorAll('.progressive-img div[data-src]')
-                Object.keys(bgImgs).forEach(k => {
-                    const el = bgImgs[k]
-                    ioBgImgs.observe(el)
-                })
-            } else {
-                const imgs = document.querySelectorAll('.progressive-img img[data-src]')
-                Object.keys(imgs).forEach(k => {
-                    const el = imgs[k]
-                    const src = el.getAttribute('data-src')
-                    el.src = src
-                })
-
-                const bgImgs = document.querySelectorAll('.progressive-img div[data-src]')
-                Object.keys(bgImgs).forEach(k => {
-                    const el = bgImgs[k]
-                    const src = el.getAttribute('data-src')
-                    el.setAttribute('style', `background-image: url('${src}');`)
-                })
-            }
+        const watch = (nodeList, observer) => {
+            Object.keys(nodeList).forEach(k => {
+                const el = nodeList[k]
+                observer.observe(el)
+            })
         }
 
-        // create callback for use with MutationObserver
-        const callback = (mutationsList, observer) => {
-            const elementsWereAdded = (list) => {
-                for (let i = 0; i < list.length; i++) {
-                    if (list[i].addedNodes && list[i].addedNodes.length > 0) {
-                        return true
+        if (typeof IntersectionObserver !== 'undefined') {
+            // regular images observer
+            const ioImgs = new IntersectionObserver(
+                entries => {
+                    // console.log(entries)
+
+                    Object.keys(entries).forEach(k => {
+                        if (!entries[k].isIntersecting) {
+                            return;
+                        }
+                        const el = entries[k].target
+                        const src = el.getAttribute('data-src')
+                        el.src = src
+                        el.removeAttribute('data-src')
+                        ioImgs.unobserve(el)
+                    })
+                },
+                {
+                    /* Using default options. */
+                }
+            )
+
+            // background images observer
+            const ioBgImgs = new IntersectionObserver(
+                entries => {
+                    // console.log(entries)
+
+                    Object.keys(entries).forEach(k => {
+                        if (!entries[k].isIntersecting) {
+                            return;
+                        }
+                        const el = entries[k].target
+                        const src = el.getAttribute('data-src')
+                        el.setAttribute('style', `background-image: url('${src}');`)
+                        el.removeAttribute('data-src')
+                        ioBgImgs.unobserve(el)
+                    })
+                },
+                {
+                    /* Using default options. */
+                }
+            )
+
+            const imgs = document.querySelectorAll('.progressive-img img[data-src]')
+            watch(imgs, ioImgs)
+
+
+
+            const bgImgs = document.querySelectorAll('.progressive-img div[data-src]')
+            watch(bgImgs, ioBgImgs)
+
+            // create callback for use with MutationObserver
+            const callback = (mutationsList, observer) => {
+                console.log('mutationsList', mutationsList)
+                for (let i = 0; i < mutationsList.length; i++) {
+                    if (mutationsList[i].addedNodes && mutationsList[i].addedNodes.length > 0) {
+                        const nodes = mutationsList[i].addedNodes
+                        Object.keys(nodes).forEach(k => {
+                            const node = nodes[k]
+                            if (node.nodeType === Node.ELEMENT_NODE) {
+                                const addedImgs = node.querySelectorAll('.progressive-img img[data-src]')
+                                watch(addedImgs, ioImgs)
+
+                                const addedBgImgs = node.querySelectorAll('.progressive-img div[data-src]')
+                                watch(addedBgImgs, ioBgImgs)
+                            }
+                        })
                     }
                 }
-                return false
             }
-            if (elementsWereAdded(mutationsList)) {
-                watch()
+
+            // if supported, add a mutation observer for all body DOM nodes
+            if (typeof MutationObserver !== 'undefined') {
+                const obs = new MutationObserver(callback)
+                const root = document.getElementsByTagName('body')[0]
+                const config = { childList: true, subtree: true };
+                obs.observe(root, config)
             }
-        }
+        } else {
+            // if IntersectionObserver is not supported, just replace all images
+            const imgs = document.querySelectorAll('.progressive-img img[data-src]')
+            Object.keys(imgs).forEach(k => {
+                const el = imgs[k]
+                const src = el.getAttribute('data-src')
+                el.src = src
+            })
 
-        // if supported, add a mutation observer for all body DOM nodes
-        if (typeof MutationObserver !== 'undefined') {
-            const obs = new MutationObserver(callback)
-            const root = document.getElementsByTagName('body')[0]
-            const config = { childList: true, subtree: true };
-            obs.observe(root, config)
+            const bgImgs = document.querySelectorAll('.progressive-img div[data-src]')
+            Object.keys(bgImgs).forEach(k => {
+                const el = bgImgs[k]
+                const src = el.getAttribute('data-src')
+                el.setAttribute('style', `background-image: url('${src}');`)
+            })
         }
-
-        // initial watch event
-        watch()
     })
 }
